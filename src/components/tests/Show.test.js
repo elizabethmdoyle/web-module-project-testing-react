806fedafb144ac2c8b3f9a505343f9d3b109a90c
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-01-09 18:25:47 -0500

    working on show tests

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,11 +3,32 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-test('renders without errors', () => {
-    render(<Show />)
+const testShow = {
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: "Season 1",
+            episodes: []
+        },    
+         {
+            id: 1,
+            name: "Season 2",
+            episodes: []
+        }
+    ]
+}
+
+test('renders testShow an no selected season without errors', () => {
+    render(<Show show={testShow} selectedSeason={"none"}/>)
  });
 
-test('renders Loading component when prop show is null', () => { });
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null}/>)
+    const loading = screen.queryByTestId('loading-container')
+    //screen.getByText(/Fetching data/i)
+ });
 
 test('renders same number of options seasons are passed in', () => { });
 
