765cf42f1352111f892d359cb182d22fa7cf0db9
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-01-09 19:57:06 -0500

    working on display

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+
+const testShow = {
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: "Season 1",
+            episodes: []
+        },    
+         {
+            id: 1,
+            name: "Season 2",
+            episodes: []
+        }
+    ]
+}
 
 test('renders without errors with no props', async () => { 
     render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders Show component when the button is clicked ', () => {
+    render(<Display />)
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
-test('renders show season options matching your data when the button is clicked', () => { });
+ });
+
+test('renders show season options matching your data when the button is clicked', () => { 
+
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
@@ -28,10 +30,30 @@ test('renders Loading component when prop show is null', () => {
     render(<Show show={null}/>)
     const loading = screen.queryByTestId('loading-container')
     //screen.getByText(/Fetching data/i)
+    expect(loading).toBeInTheDocument();
  });
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders same number of options seasons are passed in', () => {
+    render(<Show show={testShow} selectedSeason={'none'}/>)
+    const seasonOptions = screen.queryAllByTestId('season-option')
+    expect(seasonOptions).toHaveLength(2);
+ });
 
-test('handleSelect is called when an season is selected', () => { });
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>)
+    const select = screen.queryByLabelText(/Select A Season/i);
+    userEvent.selectOptions(select, ['1']);
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+    expect(handleSelect).toBeCalled();
+ });
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { 
+    const { rerender } = render(<Show show={testShow} selectedSeason={'none'}/>);
+    let episodes = screen.queryByTestId('episodes-container');
+    expect(episodes).not.toBeInTheDocument();
+    rerender(<Show show={testShow} selectedSeason={1} />)
+     episodes = screen.queryByTestId('episodes-container');
+     expect(episodes).toBeInTheDocument();
+
+});
